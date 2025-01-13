7299ad59f322b612672690b59120e78aac314f59
v <64693573+victorbianconi@users.noreply.github.com>
2025-01-13 12:52:03 +0200

    Update batch-alpha2.py

--- batch-alpha2.py
+++ batch-alpha2.py
@@ -285,8 +285,8 @@ def main():
     logging.info(f"Arguments: {args}")
     
     # Update INPUT_FOLDER and OUTPUT_FOLDER if provided via arguments
-    if args.input_dir:
-        INPUT_FOLDER = Path(args.input_dir)
+    if args["input-dir"]:
+        INPUT_FOLDER = Path(args["input-dir"])
         # If output directory not specified, set it to input directory
         if not args.output_dir:
             OUTPUT_FOLDER = INPUT_FOLDER
